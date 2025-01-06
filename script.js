6f7fbb5846e7efd4a13f88c214afb7cfad4f0862
Tommy Braes <46817244+segestaconsult@users.noreply.github.com>
2025-01-06 16:37:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const UserManager = window.Oidc.UserManager;
 
 // OpenID Connect configuration
 const oidcSettings = {
-  authority: "https://YOUR_AUTHORITY_URL", // Replace with your OpenID Provider URL
-  client_id: "YOUR_CLIENT_ID",
+  authority: "https://oauth.id.jumpcloud.com/oauth2/auth", // Replace with your OpenID Provider URL
+  client_id: "2481f242-a06f-4782-9b56-dfeaa69bfa8b",
   redirect_uri: window.location.origin + "/success.html",
   response_type: "code", // Use Authorization Code flow
   scope: "openid profile email",
